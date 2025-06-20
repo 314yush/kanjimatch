487c82e27b605b2ae117f93b3c7491b9a6c0e68e
314yush <startrek2219@gmail.com>
2025-06-20 03:28:23 -0700

    fix: vercel changes

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,7 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+function handler(req: VercelRequest, res: VercelResponse) {
   res.status(200).json({ message: 'Hello from Vercel API!' });
-} 
+}
+
+module.exports = handler; 
